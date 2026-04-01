7608a5a2c1ecf617682a7bed9aef17a23c8ce7fb
Alpha_VG <imvenugopal08@gmail.com>
2026-04-01 13:35:16 +0530

    Add URL pattern for about page view

--- Django/config/core/views.py
+++ Django/config/core/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def about(request):
+    # return render(request, 'core/about.html')
+    return HttpResponse("This is the about page")
